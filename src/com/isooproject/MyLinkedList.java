ebe70381f72bf1fadc986802c269b60738eaf2b6
leesoo <isoo7510@gmail.com>
2018-09-09 19:22:57 +0900

    LinkedList addHead 구현

--- src/com/isooproject/MyLinkedList.java
+++ src/com/isooproject/MyLinkedList.java
@@ -11,8 +11,25 @@ public class MyLinkedList<E> {
     public void addTail(E element) {
         // 아래서 constructor로 강제성 부여
         Node<E> newNode = new Node<>(element);
+        // tail 다음으로 newNode를 붙이기
         tail.setNext(newNode);
+        // tail을 newNode로
         tail = newNode;
+        // size 1증가
+        size++;
+    }
+
+    // Head 뒤에 추가하기
+    public void addHead(E element) {
+        Node<E> newNode = new Node<>(element);
+        // 임시로 head 옆에 있는 element를 가져와서 next로 지정
+        Node<E> next = head.getNext();
+        // 그 next를 newNode.setNext로 지정
+        newNode.setNext(next);
+        // head의 setNext는 newNode로 지정
+        head.setNext(newNode);
+        // 사이즈 1 증가
+        size++;
     }
 
     static class Node<E> {
